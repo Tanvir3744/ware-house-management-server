87591022d2baa720d7dd8420a4ac9093f140e8f6
tanvir3744 <biplobmohammad1@gmail.com>
2022-05-07 12:58:39 +0600

    connected with mongodb database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = 5000;
 
 //using middle ware 
@@ -10,6 +12,19 @@ app.use(express())
 app.get('/',(req, res) =>{
     res.send('server runnig on heroku')
 })
+
+//DATABASE_USERNAME = Inventory-management
+// DATABASE_PASSWORD = iDfLuk4b6gGqnUiy
+
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.1f2fl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('database connected with my new server')
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.listen(port,()=>{
     console.log(`listening to the port ${port}`)
