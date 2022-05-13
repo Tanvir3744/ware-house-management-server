47b7d89aadbff273ba8b7cb0e4104ea6bbaf468d
Tanvir3744 <biplobmohammad1@gmail.com>
2022-05-13 22:31:51 +0600

    application error resolved

---
+++ .history/index_20220513124221.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+//using middle ware 
+app.use(cors());
+app.use(express())
+
+
+
+//DATABASE_USERNAME = Inventory-management
+// DATABASE_PASSWORD = iDfLuk4b6gGqnUiy
+app.get('/', (req, res) => {
+    res.send('welcome to heroku')
+})
+app.get('/about', (req, res) => {
+    res.send('welcome to about page of this server')
+})
+
+
+app.listen(port, () => {
+    console.log(`listening to the port ${ port }`)
+})
